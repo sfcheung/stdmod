0793ec1c800129d712ceea6776b5d5bfe93aeb8c
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-06-10 13:35:38 +0800

    Update data.R
    Create a data set with a categorical covariate.

--- R/data.R
+++ R/data.R
@@ -1,7 +1,9 @@
 #'Sample dataset for testing
 #' 
-#' One IV, one moderation, two covariates, n 100
+#' One IV, one moderation, two covariates, n 500
 #' 
-"test_x_1_w_1_v_2_n_100"
+"test_x_1_w_1_v_2_n_500"
 
-
+#' One IV, one moderation, two covariates, v1 is contrinuous and cat2 is categorical. n 500
+#' 
+"test_x_1_w_1_v_1_cat1_n_500"
