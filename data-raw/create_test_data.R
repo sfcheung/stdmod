2b347dd1f0ac3296ae184e4efb0ca9ab235ed145
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-06-10 13:37:45 +0800

    Create a test data set with a nonnumeric covariate
    Create a test data set with a nonnumeric covariate

--- data-raw/create_test_data.R
+++ data-raw/create_test_data.R
@@ -2,7 +2,7 @@
 #' 
 #' 
 
-#' One IV, one moderation, two covariates, n 100
+#' One IV, one moderator, two covariates, n 500
 #' For testing
 
 set.seed(686453)
@@ -18,3 +18,25 @@ out <- lm(dv ~ iv*mod + v1 + v2, dat)
 summary(lm.beta::lm.beta(out))
 test_x_1_w_1_v_2_n_500 <- dat
 usethis::use_data(test_x_1_w_1_v_2_n_500, overwrite = TRUE)
+
+#' One IV, one moderator, two covariates, n 500
+#' One of the covariates is string variable (categorical) with three categories.
+#' For testing
+
+set.seed(64534432)
+n <- 500
+x <- rnorm(n, 15, 2)
+w <- rnorm(n, 100, 5)
+vs <- MASS::mvrnorm(n, c(v1 = 0, v2 = 0), diag(2))
+vs <- vs %*% diag(c(3, 5)) + matrix(c(10, 50), n, 2, byrow = TRUE)
+v1 <- vs[, 1]
+cat1 <- sample(c("gp1", "gp2", "gp3"), n, replace = TRUE)
+colnames(vs) <- paste0("v", seq_len(ncol(vs)))
+y <- 10 + 3*x + 5*w + 4*x*w + 2*v1 + 
+      sapply(cat1, switch, gp1 = 1, gp2 = 4, gp3 = 2) + rnorm(n, 0, 700)
+dat <- data.frame(dv = y, iv = x, mod = w, v1, cat1, stringsAsFactors = FALSE)
+out <- lm(dv ~ iv*mod + v1 + cat1, dat)
+summary(lm.beta::lm.beta(out))
+test_x_1_w_1_v_1_cat1_n_500 <- dat
+usethis::use_data(test_x_1_w_1_v_1_cat1_n_500, overwrite = TRUE)
+
