1ec1ae323d4cef427cac1944a3fecafba34f3457
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-06-13 12:06:06 +0800

    Create summary_stdmod.R
    Append nonparametric bootsrtrapping CI if present.

---
+++ R/summary_stdmod.R
@@ -0,0 +1,30 @@
+#'@title Summary method for \code{stdmod} class output
+#'
+#'@description Summary method for \code{stdmod} class output
+#'
+#'@details Summary method for \code{stdmod} class output
+#'
+#'@return
+#'  An object of class \code{summary.stdmod}, with bootstrapping confidence intervals
+#'  added if present.
+#'
+#'@param object The output of the class \code{stdmod}.
+#'@param ...  Arguments to be passed to \code{summary.lm}.
+#'
+#'@examples
+#' # "To be prepared"
+#' @export
+
+summary.stdmod <- function(object, ...) {
+  out <- stats::summary.lm(object, ...)
+  out$scaled <- object$scaled
+  out$centered <- object$centered
+  out$scaled_by <- object$scaled_by
+  out$centered_by <- object$centered_by
+  out$nboot <- object$nboot
+  if (!is.null(object$boot_ci)) {
+    out$coefficients <- cbind(out$coefficients, object$boot_ci)
+    }
+  class(out) <- c("summary.stdmod", class(out))
+  out
+  }
