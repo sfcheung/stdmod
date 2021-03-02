12affe0d77a8ea9db40b069783218be6e23904e4
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-03-02 13:23:29 +0800

    Fix the bug in stdmod_bootci
    It now correctly stores the full output from boot.

--- R/stdmod_bootci.R
+++ R/stdmod_bootci.R
@@ -4,15 +4,18 @@
 #'
 #' @details
 #' 
-#' [stdmod_bootci()] is a wrapper of [stdmod()]. It computes the bootstrapping
-#'  confidence interval of the standardized moderation effect given 
+#' [stdmod_bootci()] is a wrapper of [stdmod()]. It computes the nonparametric bootstrapping
+#'  confidence interval of the standardized moderation effect given
 #'  the \code{lm} output.
-#'
-# The standardized moderation effect.
+#' 
+#' Percentile interval from [boot::boot.ci()] is returned by this function. If other types of 
+#' confidence intervals are desired, set `full_output = TRUE` and use [boot::boot.ci()] on the 
+#' element `boot_out` in the output of this function.
 #'
 #' @param lm_out The output from \code{lm}.
 #' @param ...    Parameters to be passed to stdmod
 #' @param nboot  The number of bootstrapping samples. Default is 100
+#' @param conf The level of confidence for the confidence interval. Default is .95.
 #' @param boot_args A named list of arguments to be passed to \code{boot}. Default
 #'                 is NULL.
 #' @param full_output Whether the full output from \code{boot} is return. Default is 
@@ -42,8 +45,9 @@
 #' # Print the 95% percential confidence interval
 #' stdmod_xyw_boot$ci
 #' 
-#' # Print the boot output, default is the point estimate:
-#' stdmod_xyw_boot$boot_out
+#' # Compute the confidence interval with other settings using boot.ci.
+#' # For example, request the 90% BCa confidence interval:
+#' boot.ci(stdmod_xyw_boot$boot_out, conf = .90, type = "bca")
 #' } 
 #'
 #' @export
@@ -53,19 +57,24 @@
 #' @order 2
 
 stdmod_bootci <- function(lm_out, ...,
-                          nboot = 100, boot_args = NULL, full_output = FALSE) {
+                          nboot = 100, conf = .95,
+                          boot_args = NULL, full_output = FALSE) {
     dat <- lm_out$model
     bootfct <- create_boot(lm_out, ...)
     boot_out <- do.call(boot::boot, 
                   c(list(data = dat, statistic = bootfct, R = nboot), 
                   boot_args)) 
 #    boot_out <- boot::boot(dat, bootfct, R = nboot)
-    ci <- boot::boot.ci(boot_out, type = "perc")$percent[4:5]
-    list(ci = ci,
-         boot_out = ifelse(full_output, boot_out, NA))
+    ci <- boot::boot.ci(boot_out, conf = conf, type = "perc")$percent[4:5]
+    out <- list(ci = ci)
+    if (full_output) {
+        out$boot_out <- boot_out
+      } else {
+        out$boot_out <- NA
+      }
+    out
   }
-  
-  
+   
 create_boot <- function(lm_out, ...) {
   function(d, ind) {
 #        print(head(ind))
