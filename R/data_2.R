4f11a657171bb8ad7456aa2d24bd742118554304
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-05-01 14:54:41 +0800

    More sample datasets for testing tumble graph

--- R/data_2.R
+++ R/data_2.R
@@ -5,4 +5,13 @@
 #' Sample size is 500.
 #'
 
-"test_x_1_w_1_v_1_cat1__xw_cov_n_500"
+"test_x_1_w_1_v_1_cat1_xw_cov_n_500"
+
+#' One IV, one moderation, two covariates, v1 is continuous
+#' and cat2 is categorical. Moderator is a three-category
+#' string variable. IV and the moderator are associated.
+#'
+#' Sample size is 500.
+#'
+
+"test_x_1_w_1_v_1_cat1_xw_cov_wcat3_n_500"
