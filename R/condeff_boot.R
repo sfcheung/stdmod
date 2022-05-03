e865a2e49b3c114db41f283889d0c5a230efc467
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-05-03 22:36:56 +0800

    Add cond_effect_boot
    Tests passed, checks passed

---
+++ R/condeff_boot.R
@@ -0,0 +1,128 @@
+#' @details
+#' This function is a wrapper of [cond_effect()]. It calls [cond_effect()]
+#' once for each bootstrap sample, and then computes the nonparametric
+#' bootstrap percentile confidence intervals (Cheung, Cheung, Lau, Hui,
+#' & Vong, 2022). If the output object is the output of [std_selected()]
+#' or [std_selected_boot()], in which mean-centering and/or standardization
+#' have been conducted, they will be repeated in each bootstrap sample.
+#' Therefore, like [std_selected_boot()], it can be used for form
+#' nonparametric bootstrap confidence intervals for standardized
+#' effects, though [cond_effect_boot()] does this for the standardized
+#' conditional effects.
+#'
+#' This function intentionally does not have an argument for setting the seed
+#' for
+#' random number. Users are recommended to set the seed, e.g., using
+#' [set.seed()]
+#' before calling it, to ensure reproducibility.
+#'
+#' @return
+#' A data-frame-like object of the conditional effects. The class is
+#' `cond_effect` and the print method will print additional information of
+#' the conditional effects.
+#'
+#' @param output The output from [stats::lm()]. It can also accept the output
+#'               from
+#'               [std_selected()] or [std_selected_boot()].
+#' @param ...  Arguments to be passed to [cond_effect()].
+#' @param conf The level of confidence for the confidence interval.
+#'              Default is .95.
+#' @param nboot The number of bootstrap samples. Default is 100.
+#' @param boot_args A named list of arguments to be passed to [boot::boot()].
+#'                 Default is `NULL`.
+#' @param save_boot_est If `TRUE`, the default, the bootstrap estimates will
+#'                      be saved in the element
+#'                      `boot_est` of the output.
+#' @param full_output Whether the full output from [boot::boot()] is returned.
+#'                    Default is `FALSE`. If `TRUE`, the full output from
+#'                    [boot::boot()] will be saved in the element `boot_out`
+#'                    of the output.
+#'
+#' @author Shu Fai Cheung <https://orcid.org/0000-0002-9871-9448>
+#'
+#'
+#' @examples
+#'
+#' # Load a sample data set
+#' # It has one predictor (iv), one moderator (mod), on covariate (v1),
+#' # one categorical covariate (cat1) with three groups, and one dv (dv).
+#' dat <- test_x_1_w_1_v_1_cat1_n_500
+#'
+#' # Do a moderated regression by lm
+#' lm_raw <- lm(dv ~ iv*mod + v1 + cat1, dat)
+#' summary(lm_raw)
+#'
+#' lm_std <- std_selected(lm_raw, to_scale = ~ iv + mod, to_center = ~ iv + mod)
+#' cond_effect(lm_std, x = iv, w = mod)
+#'
+#' # Form nonparametric bootstrap confidence intervals
+#' out <- cond_effect_boot(lm_std, x = iv, w = mod, nboot = 50)
+#' out
+#'
+#' @export
+#' @describeIn cond_effect A wrapper of [cond_effect()] that forms
+#'             nonparametric bootstrap confidence intervals.
+#' @order 2
+
+cond_effect_boot <- function(output, ...,
+                              conf = .95,
+                              nboot = 100,
+                              boot_args = NULL,
+                              save_boot_est = TRUE,
+                              full_output = FALSE) {
+    dat <- stats::model.frame(output)
+    bootfct <- create_boot_cond_effect(output, ...)
+    boot_out <- do.call(boot::boot,
+              c(list(data = dat,
+                     statistic = bootfct,
+                     R = nboot),
+                     boot_args))
+    p <- length(boot_out$t0)
+    cis <- t(sapply(seq_len(p), function(x) {
+                boot::boot.ci(boot_out, conf = conf,
+                              type = "perc", index = x)$percent[4:5]
+              }))
+    colnames(cis) <- c("CI Lower", "CI Upper")
+    cond_effect_out <- cond_effect(output, ...)
+    rownames(cis) <- cond_effect_out$Level
+    attr(cond_effect_out, "boot_ci") <- cis
+    cse <- which(colnames(cond_effect_out) == "Std. Error")
+    cond_effect_out <- insert_columns(dat = cond_effect_out,
+                                      x = cis,
+                                      after = cse - 1)
+    attr(cond_effect_out, "nboot") <- nboot
+    attr(cond_effect_out, "conf") <- conf
+    tmp <- boot_out$t
+    colnames(tmp) <- cond_effect_out$Level
+    attr(cond_effect_out, "boot_est") <- tmp
+    attr(cond_effect_out, "cond_effect_boot_call") <- match.call()
+    if (full_output) {
+        attr(cond_effect_out, "boot_out") <- boot_out
+      }
+    cond_effect_out
+  }
+
+create_boot_cond_effect <- function(output, ...) {
+  function(d, ind) {
+        force(output)
+        dat_i <- d[ind, ]
+        out_i <- stats::update(output, data = dat_i)
+        cond_effect(out_i, ...)[, 3]
+      }
+  }
+
+insert_columns <- function(dat, x, after = 1) {
+    k0 <- ncol(dat)
+    names0 <- colnames(dat)
+    if (is.null(dim(x))) {
+        x <- cbind(x)
+      }
+    names1 <- colnames(x)
+    k1 <- ncol(x)
+    out <- dat
+    names2 <- c(names0[1:after], names1, names0[(after + 1):k0])
+    out[, (after + k1):(k0 + k1)] <- out[, after:k0]
+    out[, (after + 1):(after + k1)] <- x
+    colnames(out) <- names2
+    out
+  }
---
+++ tests/testthat/test_condeff_boot.R
@@ -0,0 +1,67 @@
+
+library(testthat)
+library(stdmod)
+library(boot)
+
+dat <- sleep_emo_con
+lm_out <- lm(sleep_duration ~ age + gender + emotional_stability*conscientiousness, dat)
+lm_std <- std_selected(lm_out,
+                      to_center = ~ .,
+                      to_scale = ~ .)
+out_ustd <- cond_effect(lm_out, x = "emotional_stability", w = "conscientiousness",
+                        w_from_mean_in_sd = 1.25)
+out_std <- cond_effect(lm_std, x = "emotional_stability", w = "conscientiousness")
+
+set.seed(875415)
+boot_out_ustd <- cond_effect_boot(lm_out, x = "emotional_stability", w = "conscientiousness",
+                        w_from_mean_in_sd = 1.25,
+                        nboot = 50)
+tmpfct <- function(d, i, lm_out) {
+    if (missing(i)) {
+        d_i <- d
+      } else {
+        d_i <- d[i, ]
+      }
+    lm_out_i <- update(lm_out, data = d_i)
+    out0 <- cond_effect(lm_out_i, x = "emotional_stability", w = "conscientiousness",
+                          w_from_mean_in_sd = 1.25)
+    out0[, 3]
+  }
+tmpfct(dat, lm_out = lm_out)
+set.seed(875415)
+boot_out_ustd_check <- boot(dat, tmpfct, R = 50, lm_out = lm_out)
+
+test_that("Check ustd boot est", {
+    expect_equivalent(
+        attr(boot_out_ustd, "boot_est"),
+        boot_out_ustd_check$t
+      )
+  })
+
+set.seed(875415)
+boot_out_std <- cond_effect_boot(lm_std, x = "emotional_stability", w = "conscientiousness",
+                              nboot = 50)
+tmpfct <- function(d, i, lm_out) {
+    if (missing(i)) {
+        d_i <- d
+      } else {
+        d_i <- d[i, ]
+      }
+    lm_out_i <- update(lm_out, data = d_i)
+    lm_std_i <- std_selected(lm_out_i,
+                          to_center = ~ .,
+                          to_scale = ~ .)
+    out0 <- cond_effect(lm_std_i, x = "emotional_stability", w = "conscientiousness")
+    out0[, 3]
+  }
+tmpfct(dat, lm_out = lm_out)
+out_std[, 3]
+set.seed(875415)
+boot_out_std_check <- boot(dat, tmpfct, R = 50, lm_out = lm_out)
+
+test_that("Check std boot est", {
+    expect_equivalent(
+        attr(boot_out_std, "boot_est"),
+        boot_out_std_check$t
+      )
+  })
