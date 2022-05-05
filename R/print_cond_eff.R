679bfc18b480667d109f7aec3e9b4d4d672061c2
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-05-05 21:19:07 +0800

    Add more arguments to print.cond_effect

--- R/print_cond_eff.R
+++ R/print_cond_eff.R
@@ -9,6 +9,21 @@
 #' @param nd The number of digits for the variables.
 #' @param nd_stat The number of digits for test statistics (e.g., t).
 #' @param nd_p The number of digits for p-values.
+#' @param title If `TRUE`, print a title. Default is `TRUE`.
+#' @param model If `TRUE`, print the regression model. Default is `TRUE`.
+#' @param level_info If `TRUE`, print information for interpreting the
+#'                    levels of the moderator, such as the values of
+#'                     the levels and distance from the mean. Default
+#'                      is `TRUE`.
+#' @param standardized If `TRUE` and one or more variables are
+#'                     standardized, report it. Default is `TRUE`.`
+#' @param boot_info If `TRUE` and bootstrap estimates are in `x`,
+#'                  print information about the bootstrapping,
+#'                  such as the number of bootstrap samples.
+#'                  Default is `TRUE`.
+#' @param table_only If `TRUE`, will suppress of other elements except
+#'                    for the table of conditional effects. Override
+#'                    arguments such as `title`, `model`, and `level_info`.
 #' @param ...  Arguments to be passed to [cond_effect()].
 #'
 #'
@@ -35,6 +50,12 @@ print.cond_effect <- function(x,
                               nd = 3,
                               nd_stat = 3,
                               nd_p = 3,
+                              title = TRUE,
+                              model = TRUE,
+                              level_info = TRUE,
+                              standardized = TRUE,
+                              boot_info = TRUE,
+                              table_only = FALSE,
                               ...) {
     xdf <- as.data.frame(x)
     orgcall <- attr(x, "call")
@@ -54,13 +75,14 @@ print.cond_effect <- function(x,
       } else {
         ci_bcilo <- ci_bcihi <- NA
       }
-    cat("The effects of ",
-        iv,
-        " on ",
-        y,
-        ", conditional on ",
-        w, ":\n\n", sep = "")
-
+    if (title & !table_only) {
+        cat("The effects of ",
+            iv,
+            " on ",
+            y,
+            ", conditional on ",
+            w, ":\n\n", sep = "")
+      }
     colnames(xdf)[3] <- paste0(iv, " Effect")
     colnames(xdf)[ci_se] <- "S.E."
     colnames(xdf)[ci_t] <- "t"
@@ -87,27 +109,33 @@ print.cond_effect <- function(x,
     if (has_bootci) {
         nboot <- attr(x, "nboot")
         conf <- attr(x, "conf")
-        cat("\n[CI Lower, CI Upper] shows the ",
-             round(conf* 100, 2),
-             "% nonparametric bootstrap confidence interval(s)",
-             sep = "")
-        cat("\n (based on ", nboot, " bootstrap samples)")
-        cat("\n")
+        if (boot_info &  !table_only) {
+            cat("\n[CI Lower, CI Upper] shows the ",
+                round(conf* 100, 2),
+                "% nonparametric bootstrap confidence interval(s)",
+                sep = "")
+            cat("\n (based on ", nboot, " bootstrap samples)")
+            cat("\n")
+          }
       }
 
-    cat("\nThe regression model:\n")
-    cat("\n\t", deparse(stats::formula(orgoutput)), "\n", sep = "")
+    if (model & !table_only) {
+        cat("\nThe regression model:\n")
+        cat("\n\t", deparse(stats::formula(orgoutput)), "\n", sep = "")
+      }
 
     if (w_numeric) {
         w_df <- xdf[, 1:2]
         w_df$`% Below` <- formatC(100 * attr(x, "w_empirical_percentiles"), 2, format = "f")
         w_df$`From Mean (in SD)` <- formatC(attr(x, "w_empirical_z"), 2, format = "f")
-        cat(paste0("\nInterpreting the levels of ", w, ":\n\n"))
-        print(w_df, row.names = FALSE)
-        cat("\n")
-        cat("- % Below: The percent of cases equal to or less than a level.\n")
-        cat("- From Mean (in SD): Distance of a level from the mean,\n",
-            "  in standard deviation (+ve above, -ve below).\n", sep = "")
+        if (level_info & !table_only) {
+            cat(paste0("\nInterpreting the levels of ", w, ":\n\n"))
+            print(w_df, row.names = FALSE)
+            cat("\n")
+            cat("- % Below: The percent of cases equal to or less than a level.\n")
+            cat("- From Mean (in SD): Distance of a level from the mean,\n",
+                "  in standard deviation (+ve above, -ve below).\n", sep = "")
+          }
       }
     y_std <- attr(x, "y_standardized")
     x_std <- attr(x, "x_standardized")
@@ -118,14 +146,15 @@ print.cond_effect <- function(x,
                       c(NA, NA, NA))
         v_std <- paste0(stats::na.omit(tmp), collapse = ", ")
         v_txt <- paste0("- The variable(s) ", v_std, " is/are standardized.")
-        cat("\nNote:\n\n")
-        cat(v_txt, "\n", sep = "")
-        if (y_std & x_std) {
-            cat("- The conditional effects are the standardized effects of ",
-                iv,
-                " on ",
-                y, ".", sep = "")
+        if (standardized) {
+            cat("\nNote:\n\n")
+            cat(v_txt, "\n", sep = "")
+            if (y_std & x_std) {
+                cat("- The conditional effects are the standardized effects of ",
+                    iv,
+                    " on ",
+                    y, ".", "\n", sep = "")
+              }
           }
       }
-    cat("\n")
   }
