7d8624a4cfb80ed2cd776b31aba3df14949cf673
Shu Fai Cheung <shufai.cheung@gmail.com>
2024-05-12 19:43:32 +0800

    Simplify the tests

--- tests/testthat/test_std_selected_lavaan_mg.R
+++ tests/testthat/test_std_selected_lavaan_mg.R
@@ -1,12 +1,8 @@
-skip_on_cran()
-# A long test
-
 # testthat::test_file("./tests/testthat/test_std_selected_lavaan_mg.R")
 
 library(testthat)
 library(lavaan)
 library(manymome)
-library(numDeriv)
 
 dat <- HolzingerSwineford1939
 dat$age_gp <- dat$ageyr
@@ -28,69 +24,36 @@ fit <- sem(mod,
            fixed.x = FALSE,
            group = "school")
 
-(est <- parameterEstimates(fit,
+est <- parameterEstimates(fit,
                           standardized = TRUE,
-                          ci = FALSE))
-(std <- standardizedSolution(fit))
-(std_nox <- standardizedSolution(fit, type = "std.nox"))
-(std_lv <- standardizedSolution(fit, type = "std.lv"))
+                          ci = FALSE)
+std <- standardizedSolution(fit)
+std_nox <- standardizedSolution(fit, type = "std.nox")
+std_lv <- standardizedSolution(fit, type = "std.lv")
 
 test_that("Standardized coefficient", {
-
-  i <- 2
-  fit_std_2 <- gen_std_i(fit = fit, i = i)
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
-               std[i, "est.std"])
-  expect_equal(as.vector(std_se),
-               std[i, "se"])
-
-  i <- 29
-  fit_std_2 <- gen_std_i(fit = fit, i = i)
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
+  i <- c(2, 29, 38, 31, 33)
+  fit_est <- coef(fit)
+  fit_vcov <- vcov(fit)
+  fit_std <- lapply(i, function(x) gen_std_i(fit = fit, i = x))
+  std_se <- sapply(fit_std,
+                   FUN = std_se_delta,
+                   fit_est = fit_est,
+                   fit_vcov = fit_vcov)
+  std_est <- sapply(fit_std,
+                    function(x) x(fit_est))
+  expect_equal(std_est,
                std[i, "est.std"])
   expect_equal(std_se,
                std[i, "se"])
-
-  i <- 38
-  fit_std_2 <- gen_std_i(fit = fit, i = i)
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
-               std[i, "est.std"])
-  expect_equal(std_se,
-               std[i, "se"])
-
-  i <- 31
-  fit_std_2 <- gen_std_i(fit = fit, i = i)
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
-               std[i, "est.std"])
-  expect_equal(std_se,
-               std[i, "se"])
-
-  i <- 33
-  fit_std_2 <- gen_std_i(fit = fit, i = i)
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
-               std[i, "est.std"])
-  expect_equal(std_se,
-               std[i, "se"])
-
-
 })
 
 fit2 <- sem(mod,
             dat[c(1:100, 150:250), ],
             fixed.x = FALSE,
             group = "school")
-(std2 <- standardizedSolution(fit2))
-(std2_nox <- standardizedSolution(fit2, type = "std.nox"))
+std2 <- standardizedSolution(fit2)
+std2_nox <- standardizedSolution(fit2, type = "std.nox")
 
 test_that("Alternate values", {
   fit_std_2 <- gen_std_i(fit = fit, i = 29)
@@ -101,80 +64,50 @@ test_that("Alternate values", {
 # Delta method
 
 test_that("All est", {
-  i_std <- which((std$se > 0) &
-                 (std$op != "~1"))
-  fit_std_all <- lapply(i_std,
-                        gen_std_i,
-                        fit = fit)
-  std_a <- lapply(fit_std_all, grad, x = coef(fit))
-  std_se <- sapply(std_a, function(xx) {
-                  sqrt(colSums(xx * (vcov(fit) %*% xx)))
-                  #sqrt(xx %*% vcov(fit) %*% xx)
-                })
-  expect_equal(std_se / std$se[i_std],
-               rep(1, length(std_se)))
-  fit_std_all_est <- sapply(fit_std_all, function(xx) xx(coef(fit)))
-  expect_equal(fit_std_all_est / standardizedSolution(fit)$est.std[i_std],
-               rep(1, length(fit_std_all_est)))
+  out <- std_selected_lavaan(fit, standardized = TRUE, skip_categorical_x = FALSE)
+  i <- out$op %in% c("~~", "~", "=~")
+  expect_equal(out$std.all[i],
+               out$std.p[i])
 })
 
 # (which(std_nox$est.std != std$est.std))
 #  4  5  6 11 12 13 14 15 16 20 21 22 27 28 29 34 35 36 37 38 39 43 44 45
 
 test_that("Standardized coefficients: No X", {
-
-  i <- 4
-  fit_std_2 <- gen_std_i(fit = fit, i = i, to_standardize = "f1")
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
+  i <- c(4, 28, 38)
+  fit_est <- coef(fit)
+  fit_vcov <- vcov(fit)
+  fit_std <- lapply(i, function(x) gen_std_i(fit = fit, i = x, to_standardize = "f1"))
+  std_se <- sapply(fit_std,
+                   FUN = std_se_delta,
+                   fit_est = fit_est,
+                   fit_vcov = fit_vcov)
+  std_est <- sapply(fit_std,
+                    function(x) x(fit_est))
+  expect_equal(std_est,
                std_nox[i, "est.std"])
   expect_equal(std_se,
                std_nox[i, "se"])
-
-  i <- 28
-  fit_std_2 <- gen_std_i(fit = fit, i = i, to_standardize = "f1")
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
-               std_nox[i, "est.std"])
-  expect_equal(std_se,
-               std_nox[i, "se"])
-
-  i <- 38
-  fit_std_2 <- gen_std_i(fit = fit, i = i, to_standardize = "f1")
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
-               std_nox[i, "est.std"])
-  expect_equal(std_se,
-               std_nox[i, "se"])
-
-  i <- 25
-  fit_std_2 <- gen_std_i(fit = fit, i = i, to_standardize = c("f1", "x2"))
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
-               std_nox[i, "est.std"])
-  expect_equal(std_se,
-               std_nox[i, "se"])
-
 })
 
 # (which(std_lv$est.std != std$est.std))
 #  1  2  3  4  5  6  7  8  9 11 12 13 14 15 16 17 18 19 20 21 22 24 25 26 27 28 29 30 31 32 34 35 36 37 38 39 40 41 42 43 44 45
 
 test_that("Standardized coefficients: lv", {
-
-  i <- 25
-  fit_std_2 <- gen_std_i(fit = fit, i = i, to_standardize = "f1")
-  std_a <- grad(fit_std_2, x = coef(fit))
-  std_se <- sqrt(colSums(std_a * (vcov(fit) %*% std_a)))
-  expect_equal(as.vector(fit_std_2(coef(fit))),
+  i <- c(25)
+  fit_est <- coef(fit)
+  fit_vcov <- vcov(fit)
+  fit_std <- lapply(i, function(x) gen_std_i(fit = fit, i = x, to_standardize = "f1"))
+  std_se <- sapply(fit_std,
+                   FUN = std_se_delta,
+                   fit_est = fit_est,
+                   fit_vcov = fit_vcov)
+  std_est <- sapply(fit_std,
+                    function(x) x(fit_est))
+  expect_equal(std_est,
                std_lv[i, "est.std"])
   expect_equal(std_se,
                std_lv[i, "se"])
-
 })
 
 # Automatically skip ageXX
