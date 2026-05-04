e0a67eb4730c8d098ad56403adb30cc46e42ce8c
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-05-04 21:22:45 +0800

    0.2.13: Replace 'simpleError" with "error"
    tests and checks passed.

--- R/plotmod.R
+++ R/plotmod.R
@@ -218,14 +218,14 @@ plotmod <- function(output, x, w,
     graph_type <- match.arg(graph_type)
     x0 <- deparse(substitute(x))
     if (inherits(tryCatch(x00 <- as.character(x), error = function(e) e),
-                 "simpleError")) {
+                 "error")) {
         x <- x0
       } else {
         x <- x00
       }
     w0 <- deparse(substitute(w))
     if (inherits(tryCatch(w00 <- as.character(w), error = function(e) e),
-                 "simpleError")) {
+                 "error")) {
         w <- w0
       } else {
         w <- w00
